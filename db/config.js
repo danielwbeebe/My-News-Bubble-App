f8b01431ab2fdeb9d37c82ba594db1126d6b4e53
Daniel Beebe <danwbeebe@gmail.com>
2017-07-31 15:02:31 -0400

    add code to config.js

--- db/config.js
+++ db/config.js
@@ -1,2 +1,29 @@
+// Creating constant variable options
+const options = {
+  query: (e) => {
+    console.log(e.query);
+  },
+};
 
+// requiring pg-promise
+const pgp = require('pg-promise')(options);
+
+// Setting up database and port 5432
+function setDatabase() {
+  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    return pgp({
+      database: 'sources_review_dev',
+      port: 5432,
+      host: 'localhost',
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+};
+
+// Defining contant variable db for database
+const db = setDatabase();
+
+// Exporting db module
+module.exports = db;
 
