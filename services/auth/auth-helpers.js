3e1651bad4e2b93ef4e160be44155b1b09436183
Daniel Beebe <danwbeebe@gmail.com>
2017-07-31 22:53:57 -0400

    add code to auth-helpers.js

---
+++ services/auth/auth-helpers.js
@@ -0,0 +1,26 @@
+// require bcrypt
+const bcrypt = require('bcryptjs');
+
+// check password
+function comparePass(userPassword, databasePassword) {
+  return bcrypt.compareSync(userPassword, databasePassword);
+};
+
+// redirect
+function loginRedirect(req, res, next) {
+  if (req.user) return res.redirect('/user');
+  return next();
+};
+
+// require login
+function loginRequired(req, res, next) {
+  if (!req.user) return res.redirect('/auth/login');
+  return next();
+};
+
+// exports
+module.exports = {
+  comparePass,
+  loginRedirect,
+  loginRequired,
+};
