2c9502aec41438013e4530f5b55f4d481bde03e1
Daniel Beebe <danwbeebe@gmail.com>
2017-07-31 21:57:55 -0400

    added code to sources-controller.js

---
+++ controllers/sources-controller.js
@@ -0,0 +1,72 @@
+// require models/source
+const Source = require('../models/source');
+
+// defining sources controller as object
+const sourcesController = {};
+
+// index
+sourcesController.index = (req, res) => {
+  Source.findAll(req.user.id)
+    .then(sources => {
+      res.render('sources/sources-index', {
+        data: sources,
+      });
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+    });
+};
+
+// create
+sourcesController.create = (req, res) => {
+  Source.create({
+    source: req.body.source,
+    user_id: req.user.id,
+  }).then(source => {
+    console.log(source);
+    res.redirect('/sources');
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ err });
+  });
+};
+
+// edit
+sourcesController.edit = (req, res) => {
+  Source.findById(req.params.id)
+    .then(source => {
+      res.render('sources/sources-edit', {
+        source: source,
+      })
+    }).catch(err => {
+    console.log(err);
+    res.status(500).json({ err });
+  });
+}
+
+// update
+sourcesController.update = (req, res) => {
+  Source.update({
+    source: req.body.source,
+    user_id: req.user.id,
+  }, req.params.id).then(source => {
+    res.redirect('/sources');
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ err });
+  });
+}
+
+// delete
+sourcesController.delete = (req, res) => {
+  Source.destroy(req.params.id)
+    .then(() => {
+      res.redirect('/sources');
+    }).catch(err => {
+    console.log(err);
+    res.status(500).json({ err });
+  });
+}
+
+// export module
+module.exports = sourcesController;
