4400099d05d27c37a08080aa774a075dfc6775db
Daniel Beebe <danwbeebe@gmail.com>
2017-07-31 22:07:23 -0400

    add code to user.js

---
+++ models/user.js
@@ -0,0 +1,26 @@
+// require db config
+const db = require('../db/config');
+
+// define user as object
+const User = {};
+
+// find user
+User.findByUserName = (username) => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE username = $1
+  `, [username]);
+};
+
+// create user
+User.create = (user) => {
+  return db.one(`
+    INSERT INTO users
+    (username, password_digest, email)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `, [user.username, user.password_digest, user.email]);
+};
+
+// export module
+module.exports = User;
