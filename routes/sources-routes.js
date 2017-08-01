692920a53732e0aca854404ac8b6b185258e8edc
Daniel Beebe <danwbeebe@gmail.com>
2017-08-01 09:02:40 -0400

    add header and nav to boilerplate.ejs

--- routes/sources-routes.js
+++ routes/sources-routes.js
@@ -1,22 +1,24 @@
 // require express
 const express = require('express');
-const sourcesRouter = express.Router();
+const sourcesRoutes = express.Router();
 const authHelpers = require('../services/auth/auth-helpers');
 const sourcesController = require('../controllers/sources-controller');
 
 // get and post for verification
-sourcesRouter.get('/', authHelpers.loginRequired, sourcesController.index);
-sourcesRouter.post('/', authHelpers.loginRequired, sourcesController.create);
+sourcesRoutes.get('/', authHelpers.loginRequired, sourcesController.index);
+sourcesRoutes.post('/', authHelpers.loginRequired, sourcesController.create);
 
 // new
-sourcesRouter.get('/new', authHelpers.loginRequired, (req, res) => {
-  res.render('sources/sources-add');
+sourcesRoutes.get('/new', authHelpers.loginRequired, (req, res) => {
+  res.render('sources/sources-add', {
+    currentPage: 'add',
+  });
 });
 
-sourcesRouter.get('/:id', authHelpers.loginRequired, sourcesController.show);
-sourcesRouter.get('/:id/edit', authHelpers.loginRequired, sourcesController.edit);
-sourcesRouter.put('/:id', authHelpers.loginRequired, sourcesController.update);
-sourcesRouter.delete('/:id', authHelpers.loginRequired, sourcesController.delete);
+sourcesRoutes.get('/:id', authHelpers.loginRequired, sourcesController.show);
+sourcesRoutes.get('/:id/edit', authHelpers.loginRequired, sourcesController.edit);
+sourcesRoutes.put('/:id', authHelpers.loginRequired, sourcesController.update);
+sourcesRoutes.delete('/:id', authHelpers.loginRequired, sourcesController.delete);
 
 // export module
-module.exports = sourcesRouter;
+module.exports = sourcesRoutes;
