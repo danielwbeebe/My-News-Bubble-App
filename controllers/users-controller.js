3e6c1b5961067015c5df0d7766fcbadeb728af64
Daniel Beebe <danwbeebe@gmail.com>
2017-07-31 21:46:26 -0400

    add code to users-controller.js

---
+++ controllers/users-controller.js
@@ -0,0 +1,33 @@
+// require models/user and bcrypt package
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+// define usersController as object
+const usersController = {};
+
+
+usersController.index = (req, res) => {
+  res.redirect('/sources');
+};
+
+// create users controller
+usersController.create = (req, res, next) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    username: req.body.username,
+    password_digest: hash,
+    email: req.body.email,
+  }).then(user => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/user');
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ err });
+  });
+};
+
+// export module
+module.exports = usersController;
