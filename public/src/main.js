bdca8070e0b6056ff0686e0136f5aaef2ac50396
Daniel Beebe <danwbeebe@gmail.com>
2017-08-03 16:33:52 -0400

    CSS style edits

--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,2 @@
 console.log('main.js is connected');
 
-// FUNCTION ONLY ELIMINATES BUTTON BRIEFLY  - IT RETURNS WHEN USER RETURNS TO PAGE
-function offButton() {
-  document.getElementById("button1").style.display = "none";
-};
