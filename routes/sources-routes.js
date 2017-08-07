1485555f33b6ec2306814d7f3aa20faed5542b78
Daniel Beebe <danwbeebe@gmail.com>
2017-08-06 20:42:45 -0400

    add code comments to routes

--- routes/sources-routes.js
+++ routes/sources-routes.js
@@ -6,15 +6,20 @@ const sourcesController = require('../controllers/sources-controller');
 
 const newsHelpers = require ('../services/news/news-helpers');
 
-// main root
-sourcesRoutes.get('/', authHelpers.loginRequired, sourcesController.index);
-sourcesRoutes.post('/', authHelpers.loginRequired, newsHelpers.getNewsData, sourcesController.create);
 
-// route for add view
+// root to go to index view with articles
+sourcesRoutes.get('/', authHelpers.loginRequired, sourcesController.index);
+
+// route for add view with selection of news sources
 sourcesRoutes.get('/add', authHelpers.loginRequired, sourcesController.add);
 
-// routes for specific items
+// route to post call for news article
+sourcesRoutes.post('/', authHelpers.loginRequired, newsHelpers.getNewsData, sourcesController.create);
+
+// route for specific articles
 sourcesRoutes.get('/:id', authHelpers.loginRequired, sourcesController.index);
+
+// routes for delete method
 sourcesRoutes.delete('/:id', authHelpers.loginRequired, sourcesController.delete);
 
 // export module
